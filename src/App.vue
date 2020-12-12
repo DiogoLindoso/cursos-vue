a672e1e186a4ac8853aeff67c388c32e885a0001
Diogo Lindoso <diogolindoso@gmail.com>
2020-12-12 12:01:50 -0400

    Painel de cursos parte1

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <div>
     <Navbar />
     <FormCadastro />
+    <div class="container mt-5">
+      <PainelCursos />
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar"
 import FormCadastro from "./components/FormCadastro"
+import PainelCursos from "./components/PainelCursos"
 export default {
   name: "App",
   components: {
     Navbar,
-    FormCadastro
+    FormCadastro,
+    PainelCursos
   },
   data() {
     return {
---
+++ src/components/PainelCursos.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="row" >
+        <div class="col md-3" v-for="c in cursos" :key="c.url">
+            <b-card
+                :title="c.nome"
+                :img-src="c.capa"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+                <b-card-text>
+                    {{c.url}}
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Ver Detalhes</b-button>
+            </b-card>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'PainelCursos',
+    data(){
+        return {
+            cursos:[
+                {
+                    nome: 'Curso PHP',
+                    url: 'http://curso.php',
+                    capa: 'https://picsum.photos/600/300/?image=25',
+                    descricao: 'Curso de PHP'
+                },
+                {
+                    nome: 'Curso Javascript',
+                    url: 'http://curso.javascript',
+                    capa: 'https://picsum.photos/600/300/?image=25',
+                    descricao: 'Curso de Javascript'
+                },
+            ]
+        }
+        
+    }
+}
+</script>
